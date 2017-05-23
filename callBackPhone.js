1cd350c60674131dad925fce06625ad2c7eab990
Tkachenko Sergey <re4thzdsq@gmail.com>
2017-05-24 00:22:25 +0300

    add phone mask

--- callBackPhone.js
+++ callBackPhone.js
@@ -1,31 +1,28 @@
 $(function () {
     var CALL_BACK_PHONE_POST_URL = 'https://9j741dyr4k.execute-api.eu-west-2.amazonaws.com/prod/phonesBack';
     var MESSAGE_AFTER_SAVE = 'Спасибо за ваши контактные данные';
-    var SIMPLE_PHONE_MASK = /^(\+|8)[\d\s]{10,16}$/;
 
     var callBackForm = $('#call-back-form');
     var phoneInput = callBackForm.find('#phone');
     var sendButton = callBackForm.find('button');
 
+    phoneInput.mask('+0 (000) 000 00 00', {
+        onComplete: function (cep) {
+            sendButton.prop('disabled', false);
+        },
+        onChange: function (cep){
+            if (!sendButton.prop('disabled')) {
+                sendButton.prop('disabled', true);
+            }
+        }
+    });
+
     callBackForm.on('submit', sendCallBackRequest);
-    phoneInput.on('input', enableButtonOnValidPhone);
-
-    function isValidPhone(phone) {
-        return SIMPLE_PHONE_MASK.test(phone);
-    }
-
-    function enableButtonOnValidPhone() {
-        sendButton.prop('disabled', !isValidPhone(phoneInput.val()));
-    }
 
     function sendCallBackRequest(event) {
         event.preventDefault();
 
-        var phone = callBackForm.find('#phone').val();
-
-        if (!isValidPhone(phone)) {
-            return;
-        }
+        var phone = phoneInput.val();
 
         $.ajax(CALL_BACK_PHONE_POST_URL, {
             type: 'POST',
@@ -35,6 +32,7 @@ $(function () {
             complete: function () {
                 callBackForm[0].reset();
                 phoneInput.attr('placeholder', MESSAGE_AFTER_SAVE);
+                sendButton.prop('disabled', true);
             }
         });
     }
